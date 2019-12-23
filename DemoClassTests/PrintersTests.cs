568ac7256404e7ce6891fdbfea6918c2612b28e5
clara9621 <58587888+clara9621@users.noreply.github.com>
2019-12-23 16:28:32 +0200

    Feature:Added Test that Runs

---
+++ DemoClassTests/PrintersTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoClass.Tests
+{
+    [TestClass()]
+    public class PrintersTests
+    {
+        [TestMethod()]
+        public void PrintTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
