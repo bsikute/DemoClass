bcadaf14721c210b55318d35fd0f02086a0a0588
MwangoS <natchaziya@gmail.com>
2019-12-23 16:29:15 +0200

    Feature: Added a unit test that successfully worked

---
+++ DemoClassTests/PrintersTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoClass;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoClass.Tests
+{
+    [TestClass()]
+    public class PrintersTests
+    {
+        [TestMethod()]
+        public void PrintTest()
+        {
+            Assert.AreEqual("Hello World!", Printers.Print());
+        }
+    }
+}
